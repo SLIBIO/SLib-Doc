2ef28c2a2cf2ad4807ba1f327131a6ddcdf130cd
nick_hagenov <nicoloas.hagenovo327@gmail.com>
2017-03-23 20:45:18 +0300

    update for production mode

--- server/index.js
+++ server/index.js
@@ -11,34 +11,13 @@ import webpackConfig from '../webpack.config';
 import config from './config';
 
 require('babel-polyfill');
-
-const handler = githubHookHandler({ path: '/github', secret: 'test1234' });
 if (config.env === 'development') {
-  const gitCallbackServer = express();
-  gitCallbackServer.use(cors());
-  gitCallbackServer.use((req, _res, next) => {
-    console.log('req.url', req.url);
-    handler(req, _res, (err) => {
-      const res = _res;
-      res.statusCode = 404;
-      res.end('no such location');
-    });
-    next();
-  });
-  handler.on('push', (event) => {
-    exec(path.join(__dirname, '../git_update.sh'), (error, stdout, stderr) => {
-    });
-  });
-  gitCallbackServer.listen(4000, () => console.log(chalk.green('gitCallbackServer is listening on port 4000')));
-
   const server = new WebpackDevServer(webpack(webpackConfig), {
     contentBase: '/build/',
     hot: true,
     historyApiFallback: true,
   });
   server.use(cors());
-  // Serve static resources
-  // server.use('/', express.static(path.join(__dirname, '../build')));
   server.use('/doc', express.static(path.join(__dirname, '../documentation/html')));
   server.listen(config.port, () => console.log(chalk.green(`Server is listening on port ${config.port}`)));
 } else if (config.env === 'production') {
@@ -50,3 +29,22 @@ if (config.env === 'development') {
   server.use('/doc', express.static(path.join(__dirname, '../documentation/html')));
   server.listen(config.port, () => console.log(chalk.green(`Server is listening on port ${config.port}`)));
 }
+
+
+const handler = githubHookHandler({ path: '/github', secret: 'test1234' });
+const gitCallbackServer = express();
+gitCallbackServer.use(cors());
+gitCallbackServer.use((req, _res, next) => {
+  console.log('req.url', req.url);
+  handler(req, _res, (err) => {
+    const res = _res;
+    res.statusCode = 404;
+    res.end('no such location');
+  });
+  next();
+});
+handler.on('push', () => {
+  exec(path.join(__dirname, '../git_update.sh'), (error, stdout, stderr) => {
+  });
+});
+gitCallbackServer.listen(4000, () => console.log(chalk.green('gitCallbackServer is listening on port 4000')));
